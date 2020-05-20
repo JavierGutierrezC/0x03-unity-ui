b94d2e28c6f8aebdefcd426702425b78f257825f
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-20 12:20:21 -0500

    [Fix]toggle

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void PlayMaze()
     {
-        if(colorblindMode.isOn)
+        if(colorblindMode.GetComponent<Toggle>().isOn)
         {
             trapMat.color = new Color32(255, 112, 0, 1);
             goalMat.color = Color.blue;
